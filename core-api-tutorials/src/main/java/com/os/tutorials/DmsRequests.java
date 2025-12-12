eac0f5d730986df4ec594c704a29dfbf9298a50a
Antje Oelschlägel <oelschlaegel@optimal-systems.de@AnOe-Lin-VM.optimal-systems.de>
2025-12-12 18:32:47 +0100

    new GetTutorial

--- core-api-tutorials/src/main/java/com/os/tutorials/DmsRequests.java
+++ core-api-tutorials/src/main/java/com/os/tutorials/DmsRequests.java
@@ -7,14 +7,16 @@ import okhttp3.RequestBody;
 
 import java.io.File;
 import java.lang.Object;
+import java.util.HashMap;
 
 public class DmsRequests {
 
     public static final MediaType PLAINTEXT = MediaType.parse("text/plain; charset=utf-8");
+    public static final MediaType JPEG = MediaType.parse("image/jpeg; charset=utf-8");
 
     public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
-    public static Request importPlainText(String resourcesDirectory, String metadataSource, String contentSource, MediaType contenttype) {
+    public static Request importSingleDocument(String resourcesDirectory, String metadataSource, String contentSource, MediaType contenttype) {
         RequestBody singleImportRequestBody = new MultipartBody
                 .Builder()
                 .setType(MultipartBody.FORM)
@@ -22,8 +24,8 @@ public class DmsRequests {
                         metadataSource,
                         RequestBody.create(JSON, new File(resourcesDirectory + metadataSource)))
                 .addFormDataPart("cid0 ",
-                        "schneiderlein.txt",
-                        RequestBody.create(PLAINTEXT, new File("./src/main/resources/schneiderlein.txt")))
+                        contentSource,
+                        RequestBody.create(contenttype, new File(resourcesDirectory + contentSource)))
                 .build();
 
         return new Request.Builder()
@@ -35,14 +37,46 @@ public class DmsRequests {
 
     }
 
+    public static Request batchImportTextAndImage(String resourcesDirectory, String metadataSource, String contentSource1, String contentSource2) {
+        RequestBody batchImportRequestBody = new MultipartBody
+                .Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("data",
+                        metadataSource,
+                        RequestBody.create(JSON,
+                                new File(resourcesDirectory+metadataSource)))
+                .addFormDataPart("cid1",
+                        contentSource1,
+                        RequestBody.create(PLAINTEXT,
+                                new File(resourcesDirectory+contentSource1)))
+                .addFormDataPart("cid2",
+                        contentSource2,
+                        RequestBody.create(JPEG,
+                                new File(resourcesDirectory+contentSource2)))
+                .build();
+
+        return new Request.Builder()
+                //.header("Authorization", auth)
+                .header("X-ID-TENANT-NAME", Login.tenant)
+                .url(Login.baseUrl + "/api/dms/objects")
+                .post(batchImportRequestBody)
+                .build();
+    }
+
     public static Request getMetadata(String objectId) {
         return new Request.Builder()
                 .header("Authorization", Login.auth)
                 .header("X-ID-TENANT-NAME", Login.tenant)
                 .url(Login.baseUrl+ "/api/dms/objects/" + objectId)
                 .get().build();
+    }
 
-
+    public static Request getContent(String objectId) {
+        return new Request.Builder()
+                .header("Authorization", Login.auth)
+                .header("X-ID-TENANT-NAME", Login.tenant)
+                .url(Login.baseUrl+ "/api/dms/objects/" + objectId + "/contents/file")
+                .get().build();
     }
 
     public DmsRequests() {}
--- core-api-tutorials/src/main/java/com/os/tutorials/GetTutorial.java
+++ core-api-tutorials/src/main/java/com/os/tutorials/GetTutorial.java
@@ -1,31 +1,27 @@
 package com.os.tutorials;
 
-import okhttp3.*;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Response;
+import java.io.InputStream;
+import java.io.FileOutputStream;
 
-public class ImportTutorial {
+public class GetTutorial {
 
     public static final MediaType PLAINTEXT = MediaType.parse("text/plain; charset=utf-8");
     public static final MediaType JPEG = MediaType.parse("image/jpeg; charset=utf-8");
     public static final String resourcesDirectory = "./src/main/resources/";
 
-    //Resources for Single Import
+    //Resources for Initial Import
     public static final String metadataFilename = "metadata.json";
     public static final String plainTextFilename = "geislein.txt";
-
-    //Resources for Batch Import
-    public static final String metadataBatchFilename = "metadataBatch.json";
-    public static final String contentFilename1 = "schneiderlein.txt";
-    public static final String contentFilename2 = "960px-Offterdinger_Das_tapfere_Schneiderlein.jpeg";
-
-
-
 
 
     public static void main(String[] args) {
         try {
             OkHttpClient client = Login.buildClient();
 
-            //Single Import
+            //Initial Single Import of a plain text document
             Response singleImportResponse = client.newCall(DmsRequests.importSingleDocument(resourcesDirectory, metadataFilename, plainTextFilename, PLAINTEXT)).execute();
             String singleImportResponseString = singleImportResponse.body().string();
             System.out.println(singleImportResponseString);
@@ -33,11 +29,16 @@ public class ImportTutorial {
             String objectId = DmsResponses.getObjectId(singleImportResponseString);
             System.out.println(objectId);
 
-            //Batch Import
-            Response batchImportResponse = client.newCall(DmsRequests.batchImportTextAndImage(resourcesDirectory, metadataBatchFilename, contentFilename1, contentFilename2)).execute();
-            String batchImportResponseString = batchImportResponse.body().string();
-            System.out.println(batchImportResponseString);
+            //Retrieve and Display Metadata
+            Response metadataResponse = client.newCall(DmsRequests.getMetadata(objectId)).execute();
+            String metadataResponseString = metadataResponse.body().string();
+            System.out.println(metadataResponseString);
 
+            //Retrieve and store Binary Content (as TXT file in this example)
+            Response contentResponse = client.newCall(DmsRequests.getContent(objectId)).execute();
+            InputStream in = contentResponse.body().byteStream();
+            FileOutputStream out = new FileOutputStream("downloaded_file.txt");
+            out.write(in.readAllBytes());
 
         } catch (Exception e) {
             e.printStackTrace();
